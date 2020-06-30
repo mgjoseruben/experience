9e76a2e47fb587af3c336a1087b9c36941490cee
mgjoseruben <mgjoseruben@gmail.com>
2020-06-30 08:12:09 +0200

    Update experience.php

--- experience.php
+++ experience.php
@@ -3,3 +3,25 @@
  * Plugin Name: Experience
  */
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/*
+* Do actions in plugin activation
+*/
+register_activation_hook( __FILE__ , function() {
+    if ( ! wp_next_scheduled ( 'exp_daily_event' ) ) {
+        wp_schedule_event( current_time( 'timestamp' ), 'daily', 'exp_daily_event' );
+    }
+} );
+
+/*
+* Do actions in plugin deactivation
+*/
+register_deactivation_hook( __FILE__ , function() {
+    wp_clear_scheduled_hook( 'exp_daily_event' );
+} );
+
+add_action( 'exp_daily_event', function(){
+    
+    //code to execute every day
+    
+} );
